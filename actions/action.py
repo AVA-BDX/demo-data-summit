bb37fb9daa29a511154b47229cdbb0d9e704e750
TCHALA <tchala@tchala-Aspire-E5-575G>
2021-04-16 01:36:39 +0200

    updated action.py file and docker file

--- actions/action.py
+++ actions/action.py
@@ -1,24 +1,24 @@
-# from typing import Any, Text, Dict, List
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.events import SessionStarted, ActionExecuted
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SessionStarted, ActionExecuted
 
 
 
 
-# class ActionSessionStart(Action):
-#     def name(self) -> Text:
-#         return "action_session_start"
+class ActionSessionStart(Action):
+    def name(self) -> Text:
+        return "action_session_start"
 
-#     async def run(
-#       self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]
-#     ) -> List[Dict[Text, Any]]:
-#         events = [SessionStarted()]
+    async def run(
+      self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        events = [SessionStarted()]
 
-#         dispatcher.utter_message("c'est quoi votre email ?")
+        # dispatcher.utter_message("c'est quoi votre email ?")
 
 
-#         events.append(ActionExecuted("action_listen"))
+        events.append(ActionExecuted("action_listen"))
 
-#         # the session should begin with a `session_started` event and an `action_listen`
-#         # as a user message follows
-#         return events
+        # the session should begin with a `session_started` event and an `action_listen`
+        # as a user message follows
+        return events
