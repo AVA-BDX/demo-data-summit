73b2e4a538e6bf130a023992a2b4a889f9fbbc81
Lao-Dja TCHALA <lao-dja.tchala@talan.com>
2021-09-05 23:17:42 +0200

    Delete useless nlu and make sure it is no more created from a function

--- functions/create_faq_aug_nlu_and_domain_simple.py
+++ functions/create_faq_aug_nlu_and_domain_simple.py
@@ -40,27 +40,27 @@ with open("data/all_domains/faq-aug-domain.yml", "wt", encoding="utf-8") as f:
 
 
 
-# #pas utile en local
+# # #pas utile en local
 
-#merge des deux nlu
-#combiner les deux fichiers faq-domain.yml et od-domain.yml en commun de manière automatique
-#ouverture des deux fichiers yml
-#importation de faq-nlu.yml
-with open("data/nlu/faq-nlu.yml", 'rb') as stream:
-        faq_nlu_yml = yaml.load(stream)
-#importation de faq-aug-nlu.yml
-with open("data/nlu/faq-aug-nlu.yml", 'rb') as stream:
-        faq_aug_nlu_yml = yaml.load(stream) 
-#importation de nlu.yml
-with open("data/nlu/nlu.yml", 'rb') as stream:
-        nlu_yml = yaml.load(stream) 
-for num_pivot in range(0,len(set(data_base["id_question_pivot"]))):
-    faq_aug_nlu_yml['nlu'][num_pivot]['examples'] = faq_aug_nlu_yml['nlu'][num_pivot]['examples'] + faq_nlu_yml['nlu'][num_pivot]['examples'][:-1] 
-    faq_aug_nlu_yml['nlu'][num_pivot]['examples'] = faq_aug_nlu_yml['nlu'][num_pivot]['examples'].replace("-","").split("\n")
-faq_aug_nlu_yml['nlu'].extend(nlu_yml['nlu']) 
-for intent in range(len(set(data_base["id_question_pivot"])),len(nlu_yml['nlu']) + len(set(data_base["id_question_pivot"]))):
-    faq_aug_nlu_yml['nlu'][intent]['examples'] =   faq_aug_nlu_yml['nlu'][intent]['examples'][:-1].replace("-","").encode('utf8', 'ignore').decode().replace('\x92',"'").replace('\x9c','oe').replace('\x80','€').replace('\xa0',' ').split("\n")
-with open("data/nlu.yml", 'w', encoding = "utf-8") as stream:
-    ruamel.yaml.round_trip_dump(faq_aug_nlu_yml, stream, indent=4, block_seq_indent=2)   
+# #merge des deux nlu
+# #combiner les deux fichiers faq-domain.yml et od-domain.yml en commun de manière automatique
+# #ouverture des deux fichiers yml
+# #importation de faq-nlu.yml
+# with open("data/nlu/faq-nlu.yml", 'rb') as stream:
+#         faq_nlu_yml = yaml.load(stream)
+# #importation de faq-aug-nlu.yml
+# with open("data/nlu/faq-aug-nlu.yml", 'rb') as stream:
+#         faq_aug_nlu_yml = yaml.load(stream) 
+# #importation de nlu.yml
+# with open("data/nlu/nlu.yml", 'rb') as stream:
+#         nlu_yml = yaml.load(stream) 
+# for num_pivot in range(0,len(set(data_base["id_question_pivot"]))):
+#     faq_aug_nlu_yml['nlu'][num_pivot]['examples'] = faq_aug_nlu_yml['nlu'][num_pivot]['examples'] + faq_nlu_yml['nlu'][num_pivot]['examples'][:-1] 
+#     faq_aug_nlu_yml['nlu'][num_pivot]['examples'] = faq_aug_nlu_yml['nlu'][num_pivot]['examples'].replace("-","").split("\n")
+# faq_aug_nlu_yml['nlu'].extend(nlu_yml['nlu']) 
+# for intent in range(len(set(data_base["id_question_pivot"])),len(nlu_yml['nlu']) + len(set(data_base["id_question_pivot"]))):
+#     faq_aug_nlu_yml['nlu'][intent]['examples'] =   faq_aug_nlu_yml['nlu'][intent]['examples'][:-1].replace("-","").encode('utf8', 'ignore').decode().replace('\x92',"'").replace('\x9c','oe').replace('\x80','€').replace('\xa0',' ').split("\n")
+# with open("data/nlu.yml", 'w', encoding = "utf-8") as stream:
+#     ruamel.yaml.round_trip_dump(faq_aug_nlu_yml, stream, indent=2, block_seq_indent=0)   
 
 
